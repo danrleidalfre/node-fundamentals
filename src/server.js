250f666fd851b1225d05977e485ffd8da3e5d494
Danrlei <danrleidalfre@gmail.com>
2023-11-21 19:00:32 -0300

    test res with set header and status code

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 import http from "node:http";
 
+const users = [
+  {
+    id: 1,
+    name: "Dan",
+  },
+];
+
 const server = http.createServer((req, res) => {
-  return res.end("Olá cara");
+  const { method, url } = req;
+
+  return res
+    .setHeader("Content-type", "application/json")
+    .end(JSON.stringify(users));
 });
 
 server.listen(3333);
